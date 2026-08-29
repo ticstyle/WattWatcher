ee4b61ad6b9771b978ff5096b38bead9923505f4
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-08-29 21:17:08 +0200

    Refactor async_unload_entry to use unload_ok

--- custom_components/wattwatcher/__init__.py
+++ custom_components/wattwatcher/__init__.py
@@ -1,3 +1,4 @@
+# custom_components/wattwatcher/__init__.py
 """The WattWatcher integration."""
 
 from __future__ import annotations
@@ -29,14 +30,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     # Handle clean unloading of the sensor platform
-    if (
-        await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-        and DOMAIN in hass.data
-        and entry.entry_id in hass.data[DOMAIN]
-    ):
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok and DOMAIN in hass.data and entry.entry_id in hass.data[DOMAIN]:
         hass.data[DOMAIN].pop(entry.entry_id)
 
-    return True
+    return unload_ok
 
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
